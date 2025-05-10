585eb947a22711efbbfba24987bee513364cf795
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-10 20:41:00 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -27,8 +27,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'sylivanus.silva@gmail.com',
-            password: 'Sylivanus70014316s'
+            email: 'shaddyshaddy047@gmail.com',
+            password: 'nakupendaaaaaaaaaa'
         }).ready;
         console.log('Mega storage initialized.');
         if (!fs.existsSync(credsPath)) {
